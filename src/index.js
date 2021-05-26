352c65d646a09a5ed34b2d20b0baed2385ed75f1
Aashiish-Pandey <ash006pandey@gmail.com>
2021-05-26 14:09:34 +0530

    wrapping our App component inside provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import allReducers from './reducers/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import allReducers from "./reducers/index";
+import {Provicer} from 'react-redux'
 
-const store = createStore(allReducers);
+const store = createStore(allReducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-import App from './App';
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provicer store = {store}>
+      <App />
+    </Provicer>
+
+, document.getElementById("root"));

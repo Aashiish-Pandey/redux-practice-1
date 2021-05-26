63dcd2794e54d88aedd48aa33d41fb9489dc7d1b
Aashiish-Pandey <ash006pandey@gmail.com>
2021-05-26 14:45:02 +0530

    changing state by dispatching action by using useDispatch() hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./style.css";
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
+import {increment} from './actions'
 
 export default function App() {
 
   const counter = useSelector(state=> state.counter)
   const isLogged = useSelector(state=>state.isLogged)
+  const dispatch = useDispatch()
   return (
     <div>
       <h1>Counter: {counter}</h1>
-      <div>isLooged: {isLogged}</div>
+      <button onClick = {()=>dispatch(increment())}>+</button>
+      <button>-</button>
+
+      {isLogged ?<div> user is logeed in</div>:<div> please login first</div>}
     </div>
   );
 }
